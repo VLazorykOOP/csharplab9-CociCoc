5345fb0da2be36bb4a1802611b9c2e5c336eed31
Coc <makssklyar34@gmail.com>
2024-04-21 17:13:44 +0300

    exercise 2

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,32 +1,89 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        // Виконання першого завдання
+        ReverseLinesInFile();
+
+        // Виконання другого завдання
+        GroupNumbersFromFile();
+    }
+
+    static void ReverseLinesInFile()
+    {
         string filePath = "t.txt";
 
         try
         {
-            // Читаємо вміст файлу
             string[] lines = File.ReadAllLines(filePath);
 
-            // Проходження по кожному рядку
             foreach (string line in lines)
             {
-                // Виводимо літери кожного рядка в зворотному порядку
                 for (int i = line.Length - 1; i >= 0; i--)
                 {
                     Console.Write(line[i]);
                 }
-                Console.WriteLine(); // Додаємо перехід на новий рядок після кожного рядка
+                Console.WriteLine();
             }
         }
         catch (Exception ex)
         {
-            // Обробка помилок
             Console.WriteLine("Помилка: " + ex.Message);
+        }
+    }
+
+    static void GroupNumbersFromFile()
+    {
+        string filePath = "numbers.txt";
+
+        List<int> numbers = File.ReadAllLines(filePath)
+                                 .Select(int.Parse)
+                                 .ToList();
+
+        int a = 5;
+        int b = 10;
+
+        Stack<int> inRangeStack = new Stack<int>();
+        Stack<int> lessThanAStack = new Stack<int>();
+        Stack<int> greaterThanBStack = new Stack<int>();
+
+        foreach (int num in numbers)
+        {
+            if (num >= a && num <= b)
+            {
+                inRangeStack.Push(num);
+            }
+            else if (num < a)
+            {
+                lessThanAStack.Push(num);
+            }
+            else
+            {
+                greaterThanBStack.Push(num);
+            }
+        }
+
+        Console.WriteLine("Numbers in the range [{0},{1}]:", a, b);
+        while (inRangeStack.Count > 0)
+        {
+            Console.WriteLine(inRangeStack.Pop());
+        }
+
+        Console.WriteLine("Numbers less than {0}:", a);
+        while (lessThanAStack.Count > 0)
+        {
+            Console.WriteLine(lessThanAStack.Pop());
+        }
+
+        Console.WriteLine("Numbers greater than {0}:", b);
+        while (greaterThanBStack.Count > 0)
+        {
+            Console.WriteLine(greaterThanBStack.Pop());
         }
     }
 }
