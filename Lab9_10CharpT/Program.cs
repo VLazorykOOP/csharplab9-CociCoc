27a422eb2846e405c123760af8798b50d2737070
Coc <makssklyar34@gmail.com>
2024-04-21 17:03:09 +0300

    exercise 1

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+﻿using System;
+using System.IO;
 
+class Program
+{
+    static void Main()
+    {
+        string filePath = "t.txt";
+
+        try
+        {
+            // Читаємо вміст файлу
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Проходження по кожному рядку
+            foreach (string line in lines)
+            {
+                // Виводимо літери кожного рядка в зворотному порядку
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(line[i]);
+                }
+                Console.WriteLine(); // Додаємо перехід на новий рядок після кожного рядка
+            }
+        }
+        catch (Exception ex)
+        {
+            // Обробка помилок
+            Console.WriteLine("Помилка: " + ex.Message);
+        }
+    }
+}
